0c2180ed82b148eb7ccacbaaae08a77087ba4918
Rubcs <rubcastellon@gmail.com>
2023-01-21 07:43:06 +0100

    create 0_boilerplate in TS

---
+++ 0_boilerplate/src/index.ts
@@ -0,0 +1,59 @@
+type Player = {
+  name: string;
+  position: string;
+  goals: number;
+};
+
+type Trainer = {
+  name: string;
+  age?: number;
+  teams?: number;
+};
+
+type Team = {
+  id: string | number;
+  name: string;
+  players: Player[];
+  matches: number;
+  points: number;
+  trainer: Trainer;
+};
+
+const team: Team = {
+  id: 101,
+  name: "Spain",
+  trainer: {
+    name: "Luis",
+    age: 40,
+    teams: 10,
+  },
+  players: [
+    {
+      name: "Juan",
+      position: "LD",
+      goals: 1,
+    },
+
+    {
+      name: "Alex",
+      position: "MD",
+      goals: 0,
+    },
+  ],
+  matches: 2,
+  points: 6,
+};
+
+type Members = (Trainer | Player)[];
+
+const teamMembers: (Trainer | Player)[] = [
+  {
+    name: "Juan",
+    position: "LD",
+    goals: 1,
+  },
+  {
+    name: "Luis",
+    teams: 10,
+  },
+];
